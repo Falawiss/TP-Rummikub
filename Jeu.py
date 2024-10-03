8c6327f312de53f3c854d45c0ed8900aec938d15
Falawiss <138690251+Falawiss@users.noreply.github.com>
2024-10-03 12:28:21 +0200

    Coding the Joueur class

--- Jeu.py
+++ Jeu.py
@@ -30,7 +30,7 @@ class Pioche :
     """
     Classe Pioche :
     Attributs :
-        - pioche : Liste de Tuiles
+        - pioche : liste de Tuiles
     Methodes :
         - __init__() : création de la pioche, et mélanger
         - tirer() : tirer une tuile de la pioche, et l'enlever de la liste
@@ -52,6 +52,39 @@ class Pioche :
         else :
             return None
 
-    
+class Joueur :
+    """
+    Classe Joueur :
+    Attributs :
+        - nom : string
+        - main : liste de Tuiles
+        - score : int
+    Methodes :
+        - __init__() : création du joueur
+        - tirer() : tirer une tuile de la pioche, et l'ajouter à la main
+        - maj_score() : compter le score de la main et l'ajouter au score 
+    """
+    def __init__(self, nom) :
+        if type(nom) == str :
+            self.nom = nom
+        else :
+            self.nom = chr(64 + np.random.randint(26))
+        self.main = []
+        self.score = 0
 
+
+    def tirer(self, nb_tuiles, pioche) :
+        for t in range(nb_tuiles) :
+            self.main.append(pioche.tirer())
+
+    def maj_score(self) :
+        for t in self.main :
+            if t.value == 0 :
+                self.score += 30
+            else :
+                self.score += t.value
+
+    def __str__(self) :
+        return f"{self.nom} \nscore : {self.score} \nmain : {[t.__str__() for t in self.main]}"
+    
 
