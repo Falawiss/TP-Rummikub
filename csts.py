312ac9bf39d49b5a626da64133d413f608e9e174
Falawiss <138690251+Falawiss@users.noreply.github.com>
2024-10-03 11:54:22 +0200

    Adding a constants file, for imports, and general variables

---
+++ csts.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+colors_name = ["joker", "jaune", "rouge", "bleu", "noir"]
+colors_code = {
+    'rouge': '\033[91m',
+    'jaune': '\033[93m',
+    'bleu': '\033[94m',
+    'noir': '\033[30m',
+    'joker': '\033[35m',
+    'reset': '\033[0m'  
+}
