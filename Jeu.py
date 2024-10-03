d1a4469b17ffb207b45faea549c8ff381519ab98
Falawiss <138690251+Falawiss@users.noreply.github.com>
2024-10-03 12:14:03 +0200

    Adding the Pioche class

--- Jeu.py
+++ Jeu.py
@@ -26,5 +26,32 @@ class Tuile :
     def __str__(self) :
         return f"{colors_code[self.color_name()]} {self.value} {colors_code['reset']}"
     
+class Pioche :
+    """
+    Classe Pioche :
+    Attributs :
+        - pioche : Liste de Tuiles
+    Methodes :
+        - __init__() : création de la pioche, et mélanger
+        - tirer() : tirer une tuile de la pioche, et l'enlever de la liste
+    """
+    def __init__(self) :
+        self.pioche = []
+        for i in range(2) :
+            for v in range(1,14) :
+                for c in range(1,5) :
+                    self.pioche.append(Tuile(v,c))
+            self.pioche.append(Tuile(0,0))
+        np.random.shuffle(self.pioche)
+
+    def tirer(self) :
+        tirage = self.pioche[-1]
+        self.pioche = self.pioche[:-1]
+        return tirage
+    
+pioche = Pioche()
+t = pioche.tirer()
+print(t)
+    
 
 
